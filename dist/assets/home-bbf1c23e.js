d69a876b6c1642207880466f25865e1609d683b2
shalishalila <120627566+shalishalila@users.noreply.github.com>
2023-03-11 17:30:30 +0800

    Add files via upload

---
+++ dist/assets/home-bbf1c23e.js
@@ -0,0 +1,1 @@
+import{E as e}from"./index-a5ee933b.js";const o=(n=6)=>e("/home/brand","get",{limit:n}),r=()=>e("/home/banner","get"),s=()=>e("/home/new","get"),a=()=>e("home/hot","get"),d=()=>e("home/goods","get"),c=()=>e("home/special","get");export{o as a,d as b,c,s as d,a as e,r as f};
